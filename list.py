63802aa4da8982ca27c877a75b9fd303b87cd1f4
8904537204 <101116873+8904537204@users.noreply.github.com>
2022-05-18 15:43:07 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,8 @@
+import os
+list1 = [8, 9, 3, 6, 1, 10]
+list1.reverse()
+print("the reversed list is",list1)
+
+list2 = [91,67,120,34,76,54,78,87,56,64,345]
+list2.sort()
+print("the sorted list is",list2)
